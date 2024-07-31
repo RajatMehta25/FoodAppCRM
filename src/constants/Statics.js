f79c4ea2f8862e3b8a6f015d1d65c783261b032b
RajatMehta25 <85251518+RajatMehta25@users.noreply.github.com>
2024-07-31 16:34:11 +0530

    Update Statics.js

--- src/constants/Statics.js
+++ src/constants/Statics.js
@@ -2,4 +2,3 @@
 // export const API_URL = "http://18.118.117.95:3000/api";
 export const API_URL = "http://43.204.209.67:3000/api";
 
-// export const GOOGLE_MAP_API_KEY = "AIzaSyANFvytlUdoF9WVD7zmQm5N9UhTF2FSjjc";
